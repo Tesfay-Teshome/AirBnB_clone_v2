23939d3c774110726c3286648441761991becb5e
Tesfay-Teshome <tesfix2017@gmail.com>
2022-10-27 07:14:25 -0700

    Deploy Archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,27 +6,47 @@ from os.path import exists
 
 env.hosts = ['44.200.170.122', '54.237.2.122']
 
-env.user = "ubuntu"
-
-
 def do_deploy(archive_path):
-    """ deploy package """
-    if archive_path is None or not os.path.isfile(archive_path):
-        print("NOT PATH")
+    """Deployes the archive to the webserver"""
+    if not exists(archive_path):
         return False
 
-    aname = os.path.basename(archive_path)
-    rname = aname.split(".")[0]
+    full_name = archive_path.split("/")[1]
+    file_name = archive_path.split("/")[1].split(".")[0]
 
-    put(local_path=archive_path, remote_path="/tmp/")
-    run("mkdir -p /data/web_static/releases/{}".format(rname))
-    run("tar -xzf /tmp/{} \
-        -C /data/web_static/releases/{}".format(aname, rname))
-    run("rm /tmp/{}".format(aname))
-    run("rm -rf /data/web_static/current")
-    run("ln -fs /data/web_static/releases/{}/ \
-        /data/web_static/current".format(rname))
-    run("mv /data/web_static/current/web_static/* /data/web_static/current/")
-    run("rm -rf /data/web_static/curren/web_static")
+    if put(archive_path, "/tmp/{}".format(
+           full_name)).failed is True:
+        return False
+
+    if run("rm -rf /data/web_static/releases/{}/".format(
+           file_name)).succeeded is False:
+        return False
+
+    if run("mkdir -p /data/web_static/releases/{}/".format(
+           file_name)).succeeded is False:
+        return False
+
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
+           full_name, file_name)).succeeded is False:
+        return False
+
+    if run("rm /tmp/{}".format(full_name)).failed is True:
+        return False
+
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(file_name, file_name)
+           ).failed is True:
+        return False
+
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(file_name)).failed is True:
+        return False
+
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(file_name)).succeeded is False:
+        return False
 
     return True
