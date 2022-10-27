93c3819ed5470a1d0db0dc410ff8efa2f1bc8a4b
Tesfay-Teshome <tesfix2017@gmail.com>
2022-10-27 11:17:19 +0000

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,48 +1,16 @@
 #!/usr/bin/env bash
-# This bash script sets up the nginx web server for the deployement of webstatic
-
-apt-get update
+# Prepare Web Server
+apt-get update -y
 apt-get install nginx -y
-
-mkdir --parents /data/web_static/shared/
-mkdir --parents /data/web_static/releases/test/
-
-echo -e "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
-
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+rm -rf /data/web_static/current
 ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+loc="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n"
 
-echo -e "\nserver {
-
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    root   /var/www/html;
-    index  index.html;
-    add_header X-Served-By $HOSTNAME;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html;
-    }
-
-    location /redirect_me {
-		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-        root /var/www/html;
-        internal;
-    }
-
-}" > /etc/nginx/sites-available/default
-
+sudo sed -i "38i $loc" /etc/nginx/sites-available/default
 service nginx restart
+exit 0
