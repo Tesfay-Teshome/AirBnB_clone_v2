1a54043f26903f86afc06558a8f0239e0548a01a
Tesfay-Teshome <tesfix2017@gmail.com>
2022-11-13 18:11:35 +0300

    number route

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,35 +1,40 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application Python is Cool"""
+"""  a script that starts a Flask web application """
+
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ Prints a Message when / is called """
-    return 'Hello HBNB!'
+def index():
+    """ index """
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Prints a Message when /hbnb is called """
-    return 'HBNB'
+    """ hbnb route """
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    """ Prints a Message when /c is called """
-    return "C " + text.replace('_', ' ')
+def c(text):
+    """ /c route """
+    return "C %s" % text.replace("_", " ")
 
 
-@app.route('/python', strict_slashes=False)
+@app.route('/python', defaults={"text": "is cool"}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_is_cool(text='is_cool'):
-    """ Prints a Message when /python is called """
-    return "Python " + text.replace('_', ' ')
+def python(text):
+    """ python route """
+    return "Python %s" % text.replace("_", " ")
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def is_n_number(n):
-    """ Prints a Message when /number is called only if n is an int"""
-    return "{:d} is a number".format(n)
+def number(n):
+    """ number route """
+    return "%d is a number" % n
+
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 5000)
